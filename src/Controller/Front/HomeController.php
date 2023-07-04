62734390142f3f5c231cfcd25ad6dc1340993bc7
belbeche <wbelbeche.s@gmail.com>
2023-07-04 19:40:57 +0200

    added calendly

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -3,9 +3,10 @@
 namespace App\Controller\Front;
 
 use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
