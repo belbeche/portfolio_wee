534d620c986c3339d3e4e0134d3db7607f03e166
belbeche <wbelbeche.s@gmail.com>
2023-07-18 00:13:07 +0200

    bug in prod

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 
 /**
