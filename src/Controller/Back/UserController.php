8218da0a0ed2c02175461e0c4a3912b0496e5c75
belbeche <wbelbeche.s@gmail.com>
2023-08-14 23:44:17 +0200

    fixed userController password hasher

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -8,6 +8,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -31,10 +32,14 @@ class UserController extends AbstractController
     /**
      * @Route("/new", name="back_user_new", methods={"GET","POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $hasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
+        $user->setPassword($hasher->hashpassword(
+            $user,
+            $form->get('password')->getData()
+        ));
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -62,12 +67,16 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="back_user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager,UserPasswordHasherInterface $hasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($hasher->hashpassword(
+                $user,
+                $form->get('password')->getData()
+            ));
             $entityManager->flush();
 
             return $this->redirectToRoute('back_user_index');
