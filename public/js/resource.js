87bf81c904b2c30cddeed0508015935a6599abae
belbeche <wbelbeche.s@gmail.com>
2024-04-08 03:45:48 +0200

    wip: fixing api pterodactyl in scriptzenit.fr

---
+++ public/js/resource.js
@@ -0,0 +1,40 @@
+(() => {
+    let getResource = async () => {
+        const resourcesDiv = document.querySelector('.await-resources')
+
+        console.log(resourcesDiv)
+
+        await axios.get('https://scriptzenit.fr/api/resources')
+            .then(response => {
+                const resources = response.data.resources
+
+                for (let resource of resources) {
+                    resourcesDiv.innerHTML += renderResources(resource)
+                }
+            })
+            .catch(error => console.log(error))
+    }
+
+    let renderResources = (resources) => {
+        return `
+            <div>
+              <div>
+                    <ul class="list-unstyled">
+                      <li>Nom du serveur : ${resources.name}</li>
+                      <li>wordpress : ${resources.wordpress}</li>
+                      <li>CPU : ${resources.limits.cpu}</li>
+                      <li>Stockage : ${resources.limits.disk}</li>
+                      <li>RAM : ${resources.limits.memory}</li>
+                      <li>Date de création : ${resources.created_at}</li>
+                      <li>Date de modification : ${resources.updated_at}</li>
+                      <li>Statut : ${resources.status}</li>
+                      <li>Service : ${resources.suspended} </li>
+                      <!--<li>Sauvegarde : backups</li>-->
+                    </ul>
+                </div>
+            </div>
+        `
+    }
+
+    getResource()
+})()
