a535b519165f83e19c4a912e9cc746a1388425aa
belbeche <wbelbeche.s@gmail.com>
2024-05-08 13:26:13 +0200

    fixed password hacher

--- src/Controller/Front/DevisController.php
+++ src/Controller/Front/DevisController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
